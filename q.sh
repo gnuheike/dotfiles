7b5a8debc72e3bc35146a6377e2a5ecd6e8bffb3
Dmitry R <gnuheike@gmail.com>
2023-09-16 10:52:56 +0300

    Create q.sh

---
+++ q.sh
@@ -0,0 +1,9 @@
+wget -qO - https://regolith-desktop.org/regolith.key | \
+gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_0-ubuntu-lunar-amd64 lunar main" | \
+sudo tee /etc/apt/sources.list.d/regolith.list
+
+sudo apt update
+sudo apt install regolith-desktop regolith-session-flashback regolith-look-lascaille
